2e887215e42087a22dad3ded2e2b7acc06d43da5
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-03-02 11:20:26 +0700

    Add viewBinding

--- app/src/main/java/masterous/if4b/kamus/activities/MainActivity.java
+++ app/src/main/java/masterous/if4b/kamus/activities/MainActivity.java
@@ -3,14 +3,26 @@ package masterous.if4b.kamus.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import masterous.if4b.kamus.R;
+import masterous.if4b.kamus.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
