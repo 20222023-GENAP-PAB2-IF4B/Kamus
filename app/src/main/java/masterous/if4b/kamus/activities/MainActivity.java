8d890e810852426fe9c92cd2ce8a67d19942b6c1
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-03-08 10:31:05 +0700

    Update MainActivity.java

--- app/src/main/java/masterous/if4b/kamus/activities/MainActivity.java
+++ app/src/main/java/masterous/if4b/kamus/activities/MainActivity.java
@@ -1,15 +1,23 @@
 package masterous.if4b.kamus.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 import masterous.if4b.kamus.R;
+import masterous.if4b.kamus.adapters.KamusViewAdapter;
+import masterous.if4b.kamus.databases.KamusHelper;
 import masterous.if4b.kamus.databinding.ActivityMainBinding;
+import masterous.if4b.kamus.models.Kamus;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private KamusHelper kamusHelper;
+    private KamusViewAdapter kamusViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +26,25 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        kamusHelper = new KamusHelper(this);
+        kamusViewAdapter = new KamusViewAdapter();
+        binding.rvKamus.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvKamus.setAdapter(kamusViewAdapter);
+
+        getAllData();
+
         binding.btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
+    }
+
+    private void getAllData() {
+        kamusHelper.open();
+        ArrayList<Kamus> kamusArrayList = kamusHelper.getAllData();
+        kamusHelper.close();
+        kamusViewAdapter.setData(kamusArrayList);
     }
 }
